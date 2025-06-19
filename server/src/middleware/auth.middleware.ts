9d31b067dc0c375f0405e58585c1f53e46eee494
tonybnya <nya.tony2010@gmail.com>
2025-06-19 17:34:58 +0100

    feat: add authentication middleware

---
+++ server/src/middleware/auth.middleware.ts
@@ -0,0 +1,36 @@
+/**
+ * Script Name : auth.middleware.ts
+ * Description : This file contains the authentication middleware
+ * Author      : @tonybnya
+ */
+
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export interface AuthRequest extends Request {
+  user?: { userId: string };
+}
+
+export const authenticate = (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res.status(401).json({ message: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, JWT_SECRET) as { userId: string };
+    req.user = { userId: decoded.userId };
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
