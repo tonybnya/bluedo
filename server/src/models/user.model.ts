04a06f3b13cd529b62e2824251f8fe34015106b5
tonybnya <nya.tony2010@gmail.com>
2025-06-19 17:16:11 +0100

    feat: add definition of user model

---
+++ server/src/models/user.model.ts
@@ -0,0 +1,39 @@
+/**
+ * Script Name : user.model.ts
+ * Description : Definition of user model
+ * Author      : @tonybnya
+ */
+
+import mongoose, { Document, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+export interface IUser extends Document {
+  username: string;
+  password: string;
+  comparePassword: (password: string) => Promise<boolean>;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre<IUser>("save", async (next) => {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.comparePassword = async (password: string) => {
+  return await bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model<IUser>("user", userSchema);
+export default User;
