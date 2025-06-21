86fe321ad52026489085d4858a3574dc5bd09a80
tonybnya <nya.tony2010@gmail.com>
2025-06-21 02:35:12 +0100

    fix: update app.ts and add frontend url

--- server/src/app.ts
+++ server/src/app.ts
@@ -27,7 +27,11 @@ connectToDatabase()
 // apply middlewares
 app.use(
   cors({
-    origin: ["http://localhost:4200", "http://127.0.0.1:4200"],
+    origin: [
+      "https://tonybnya-bluedo.vercel.app",
+      "http://localhost:4200",
+      "http://127.0.0.1:4200",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
