07bd6996f08ccf2d82fdbc496226fa8a1a0fcd34
tonybnya <nya.tony2010@gmail.com>
2025-06-21 03:07:34 +0100

    update

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: "https://tonybnya-bluedo-api.onrender.com", // to be changed to the correct prod one
+  apiUrl: "https://tonybnya-bluedo-api.onrender.com/api", // to be changed to the correct prod one
 };
