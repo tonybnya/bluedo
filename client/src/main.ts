58e01b643ee70b461ea2107debfbd18c039644c7
tonybnya <nya.tony2010@gmail.com>
2025-06-21 01:10:05 +0100

    fix: update application bootstrap configuration

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,12 @@
 import { bootstrapApplication } from "@angular/platform-browser";
 import { AppComponent } from "./app/app.component";
 import { provideRouter } from "@angular/router";
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import routeConfig from "./app/routes";
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(routeConfig)],
+  providers: [
+    provideRouter(routeConfig),
+    provideHttpClient(withInterceptorsFromDi())
+  ],
 }).catch((err) => console.error(err));
